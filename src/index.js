0c9987b78c8eec93393b4045107863439828637d
+ <+>
2023-06-21 15:59:33 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+// Wait for the DOM to load
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('create-task-form');
+  const taskInput = document.getElementById('new-task-description');
+  const taskList = document.getElementById('tasks');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault(); // Prevent form submission
+
+    const task = taskInput.value; // Get the task description from the input field
+
+    
+      // Create a new list item and append the task description
+      const listItem = document.createElement('li');
+      listItem.textContent = task;
+
+      // Append the new list item to the task list
+      taskList.appendChild(listItem);
+
+      // Clear the input field
+      form.reset()
+    // }
+  });
 });
